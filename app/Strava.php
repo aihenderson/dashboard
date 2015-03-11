e0799bd5ce3af64cb8a1f91f631e899b3c9d8b27
Andrew <andrew@aihenderson.com>
2015-03-10 21:21:39 -0400

    workgin on strava

--- app/Strava.php
+++ app/Strava.php
@@ -39,13 +39,36 @@ class Strava {
       $client = new Client($service);
 
       $athlete = $client->getAthlete();
-      print_r($athlete);
+//      dd($athlete);
 
       $activities = $client->getAthleteActivities();
-      print_r($activities);
+//      dd($activities);
 
-      $club = $client->getClub(9729);
-      print_r($club);
+      $feed = $client->getFeed();
+      foreach($feed as $activity){
+        echo '<div>';
+        echo '<p class="id">'.$activity["id"].'</p>';
+        echo '<p class="athleteId">'.$activity["athlete"]["id"].'</p>';
+        echo '<p class="athleteFirstname">'.$activity["athlete"]["firstname"].'</p>';
+        echo '<p class="athleteLastname">'.$activity["athlete"]["lastname"].'</p>';
+        echo '<img class="athleteProfile" src="'.$activity["athlete"]["profile"].'"/>';
+        echo '<p class="name">'.$activity["name"].'</p>';
+        echo '<p class="distance">'.$activity["distance"].'</p>';
+        echo '<p class="moving_time">'.$activity["moving_time"].'</p>';
+        echo '<p class="elapsed_time">'.$activity["elapsed_time"].'</p>';
+        echo '<p class="total_elevation_gain">'.$activity["total_elevation_gain"].'</p>';
+        echo '<p class="start_date_local">'.$activity["start_date_local"].'</p>';
+        echo '<p class="location_city">'.$activity["location_city"].'</p>';
+        echo '<p class="location_state">'.$activity["location_state"].'</p>';
+        echo '<p class="location_country">'.$activity["location_country"].'</p>';
+        echo '<p class="photo_count">'.$activity["photo_count"].'</p>';
+        echo '<p class="average_speed">'.$activity["average_speed"].'</p>';
+        echo '<p class="max_speed">'.$activity["max_speed"].'</p>';
+        echo '</d>';
+      }
+      die();
+//      dd($feed);
+
     } catch(Exception $e) {
       print $e->getMessage();
     }
